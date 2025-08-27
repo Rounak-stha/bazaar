41181f8822e69431208468ae1c9401a68c129f38
Rounak Shrestha <66557682+Rounak-stha@users.noreply.github.com>
2025-08-28 00:06:29 +0545

    Fix Checkout Page Responsiveness

--- src/collections/ShopLayout/Checkout/variants/OneStepWithSummary/index.tsx
+++ src/collections/ShopLayout/Checkout/variants/OneStepWithSummary/index.tsx
@@ -4,12 +4,11 @@ import { CheckoutPageVariantProps } from '@/types/checkout'
 
 export const OneStepWithSummary: FC<CheckoutPageVariantProps> = async ({ paymentProviders }) => {
   return (
-    <div className="relative container">
+    <div className="container">
       <div className="pt-4 pb-24">
         <h2 className="sr-only">Checkout</h2>
         <CheckoutForm paymentProviders={paymentProviders} />
       </div>
-      <div className="absolute top-1/2 left-1/2 -z-10 h-full min-h-dvh w-screen -translate-1/2 bg-gray-50"></div>
     </div>
   )
 }
